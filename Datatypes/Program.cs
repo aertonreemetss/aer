5b5238b71e3f5f4082a610eff27f67f55c95f104
aertonreemetss <69824722+aertonreemetss@users.noreply.github.com>
2025-02-26 11:25:26 +0200

    datatypess

---
+++ Datatypes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //taisnumber
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber)
+        }
+    }
+}
