7b1e247fcf8c540941cceedded2d6cbcac7b2258
aertonreemetss <69824722+aertonreemetss@users.noreply.github.com>
2025-02-26 11:39:53 +0200

    datatypes

--- Datatypes/Program.cs
+++ Datatypes/Program.cs
@@ -8,7 +8,33 @@
 
             //taisnumber
             int bigNumber = 1234567890;
-            Console.WriteLine(bigNumber)
+            Console.WriteLine(bigNumber);
+
+            //komaga number ??
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
+
+            //tahemark
+            string stringVar = "owaldmwaldmawk(     dwmkdmawd";
+            Console.WriteLine(stringVar);
+
+            float floatVariable = 123.123f;
+            Console.WriteLine(floatVariable);
+
+            char charVariable = '2';
+            Console.WriteLine(charVariable);
+
+            bool boolVariable = false;
+            Console.WriteLine(boolVariable);
+
+
+
+
+
+
+
+
+
         }
     }
 }
